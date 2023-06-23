9fa733cfb6c778e8884b19d90145ecee2af25289
Young Alien <itsyoungalien@gmail.com>
2023-06-23 10:01:41 -0600

    subiendo archivos

---
+++ Pruebas/TxtReversa.java
@@ -0,0 +1,21 @@
+package Pruebas;
+import java.util.Scanner;
+
+public class TxtReversa{
+    public static void main(String args[]){
+       String txtOriginal = "", txtReversa = "";
+       int i = 0;
+       Scanner in = new Scanner(System.in);
+
+       System.out.println("Ingresa el texto:");
+       txtOriginal = in.nextLine();
+    
+       i = txtOriginal.length();
+       while(i != 0){
+        txtReversa += txtOriginal.substring(i-1, i);
+        i--;
+       }
+       System.out.println("Texto en reversa:");
+       System.out.print(txtReversa);
+    }
+}
